8c507684d76286a342a906a0f8b19b8acf902f54
DESKTOP-7467K81\mayta <mayta51654944@gmail.com>
2022-07-16 18:49:34 +0800

    no message

--- 1. Two Sum/1. Two Sum.cpp
+++ 1. Two Sum/1. Two Sum.cpp
@@ -5,10 +5,10 @@ public:
         for(int i = 0 ; i < (len - 1) ; i++){
             for(int j = i + 1 ; j < len ; j++){
                 if(target == nums[i] + nums[j]){
-                    return{i, j};
+                    return {i, j};
                 }
             }
         }
-        return{};
+        return {};
     }
 };
---
+++ 1. Two Sum/2. Two Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>map;
+        for(int i = 0 ; i < nums.size() ; i++){
+            map[nums[i]] = i;
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(map.find(target-nums[i]) != map.end() && map[target-nums[i]] != i){
+                return {i, map[target-nums[i]]};
+            }
+        }
+        return {};
+    }
+};
