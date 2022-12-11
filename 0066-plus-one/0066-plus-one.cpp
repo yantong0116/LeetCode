e4664379fc9bf58aa35dd0e79aa727ae15f9ccb1
Yan <51469882+yantong0116@users.noreply.github.com>
2022-12-11 23:00:20 +0800

    Time: 8 ms (9.73%), Space: 8.7 MB (85.96%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int s=digits.size()-1;    //Length of the vector 
+        while(s>=0){
+            if(digits[s]<9){
+                digits[s]+=1;
+                return digits;
+            }
+            else{
+                digits[s]=0;
+                s--;
+            }
+        }
+        vector<int> :: iterator iter=digits.begin();
+        digits.insert(iter,1);
+        return digits;
+    }
+};
