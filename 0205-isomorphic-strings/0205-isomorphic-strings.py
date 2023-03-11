e63c35cda6cac0b7f79c19c15579561aa64b11c1
Yan <51469882+yantong0116@users.noreply.github.com>
2023-03-11 12:52:40 +0800

    Time: 26 ms (80.94%), Space: 14.8 MB (59.04%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        s2t = dict()
-        t2s = dict()
-        for i in range(len(s)):
-            if s[i] not in s2t:
-                if t[i] not in t2s:
-                    s2t[s[i]] = t[i]
-                    t2s[t[i]] = s[i]
-                else:
-                    return False
-            elif s2t[s[i]] != t[i]:
+        # Dictionary to house pairs:
+        pairs = {}
+
+        # Begin comparing chars:
+        for i in range(0, len(s)):
+            # Check if the pair has been included as a val.
+            if s[i] not in pairs and t[i] not in pairs.values():
+                pairs.update({s[i]: t[i]})
+            elif s[i] in pairs and t[i] == pairs.get(s[i]):
+                pass
+            else:
                 return False
-        
         return True
