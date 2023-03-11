89502e3c4d4ee43e925b033dfd905c5897d863d3
Yan <51469882+yantong0116@users.noreply.github.com>
2023-03-11 14:50:33 +0800

    Time: 0 ms (100.00%), Space: 5.5 MB (98.63%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.c
+++ 0392-is-subsequence/0392-is-subsequence.c
@@ -1,13 +1,13 @@
-bool isSubsequence(char *s, char *t) {
+bool isSubsequence(char * s, char * t){
     int i = 0, j = 0;
     int m = strlen(s), n = strlen(t);
-
-    while (i < m && j < n) {
-        if (s[i] == t[j]) {
+    
+    while(i < m && j < n){
+        if(s[i] == t[j]){
             i++;
         }
         j++;
     }
-
     return i == m;
 }
+
