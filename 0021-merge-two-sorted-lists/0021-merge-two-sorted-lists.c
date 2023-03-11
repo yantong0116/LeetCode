7d715b42f7758126e2f3135d24e7328e07993870
Yan <51469882+yantong0116@users.noreply.github.com>
2023-03-11 16:03:33 +0800

    Time: 0 ms (100.00%), Space: 6.2 MB (38.18%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -18,14 +18,12 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
         if (list1->val < list2->val) {
             temp->next = list1;
             list1 = list1->next;
-            temp = temp->next;
         } else {
             temp->next = list2;
             list2 = list2->next;
-            temp = temp->next;
         }
+        temp = temp->next;
     }
-
     if (list1) {
         temp->next = list1;
     }
