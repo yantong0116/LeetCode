c104ffff91f3d1bde4dae3c833713826336c729d
Yan <51469882+yantong0116@users.noreply.github.com>
2023-03-12 12:41:51 +0800

    Time: 12 ms (73.95%), Space: 8.1 MB (92.86%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.c
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.c
@@ -11,19 +11,30 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-void dfs(struct Node* root, int* ans, int* i) {
-    if (!root) {
+int countNodes(struct Node * root) {
+    int c = 0;
+    if (root == NULL)
+        return 0;
+    for (int i = 0; i < root->numChildren; i++)
+        c = c + countNodes(root->children[i]);
+    return (c + 1);
+}
+
+void recur(struct Node * root, int *v, int *pos) {
+    int i;
+    if (root == NULL)
         return;
-    }
-    ans[(*i)++] = root->val;
-    for (int j = 0; j < root->numChildren; j++) {
-        dfs(root->children[j], ans, i);
-    }
+    v[*pos] = root->val;
+    *pos = *pos + 1;
+    for (i = 0; i < root->numChildren; i++)
+        recur(root->children[i], v, pos);
+    return;
 }
 
 int* preorder(struct Node* root, int* returnSize) {
-    int* ans = malloc(sizeof(int) * 10000);
-    *returnSize = 0;
-    dfs(root, ans, returnSize);
-    return ans;
+    *returnSize = countNodes(root);
+    int* result = (int*)malloc(*returnSize * sizeof(int));
+    int pos = 0;
+    recur(root, result, &pos);
+    return result;
 }
