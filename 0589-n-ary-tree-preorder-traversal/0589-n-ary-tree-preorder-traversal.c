628d4a13f82df0b51dd634088596b2be39b8074b
Yan <51469882+yantong0116@users.noreply.github.com>
2023-03-12 12:30:40 +0800

    Time: 14 ms (50.42%), Space: 8.6 MB (45.38%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.c
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.c
@@ -1,57 +1,29 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
 
-    Node() {}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 
-    Node(int _val) {
-        val = _val;
+void dfs(struct Node* root, int* ans, int* i) {
+    if (!root) {
+        return;
     }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
+    ans[(*i)++] = root->val;
+    for (int j = 0; j < root->numChildren; j++) {
+        dfs(root->children[j], ans, i);
     }
-};
-*/
+}
 
-// class Solution {
-// public:
-//     vector<int> preorder(Node* root) {
-//         vector<int> result;
-        
-//         vector<int> recur(Node* curr){
-//             if(curr){
-//                 result.push_back(curr->val);
-//                 for(Node* child: curr->children){
-//                     recur(child);
-//                 }
-//             }
-//         }
-//         recur(root);
-//         return result;
-//     }
-// };
-
-class Solution {
-public:
-    vector<int> preorder(Node* root) {
-        vector<int> result;
-        recur(root, result);
-        return result;
-    }
-    
-    void recur(Node* curr, vector<int>& res) {
-        if (curr) {
-            res.push_back(curr->val);
-            for (Node* child : curr->children) {
-                recur(child, res);
-            }
-        }
-    }
-};
-
-
+int* preorder(struct Node* root, int* returnSize) {
+    int* ans = malloc(sizeof(int) * 10000);
+    *returnSize = 0;
+    dfs(root, ans, returnSize);
+    return ans;
+}
