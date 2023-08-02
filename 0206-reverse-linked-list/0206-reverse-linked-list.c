3ff01dc71716e6140301237b4f5f964cb0a8394f
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-02 21:12:51 +0800

    Time: 0 ms (100.00%), Space: 6.2 MB (76.82%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.c
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -7,16 +7,15 @@
  */
 
 struct ListNode* reverseList(struct ListNode* head){
-    struct ListNode *prev = NULL;
-    struct ListNode *curr = head;
-    
-    while(curr){
-        struct ListNode *next = curr->next;
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+
+    while (curr) {
+        struct ListNode* next = curr->next;
         curr->next = prev;
         prev = curr;
         curr = next;
     }
-    
     return prev;
 }
 
