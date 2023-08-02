e27199bcfd9b887ca85424673c1b74d441e54535
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-02 18:04:06 +0800

    Time: 139 ms (96.13%), Space: 109.6 MB (71.37%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,41 @@
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& nums, int k) {
+//         double result = 0;
+//         double temp = 0;
+        
+//         for(int i = 0; i < k; i++) result += nums[i];
+        
+//         result /= k;
+        
+//         for(int i = 1; i <= nums.size() - 1; i++){
+//             temp = temp - nums[i-1] + nums[i+k-1];
+//             result = max(result, temp / k);
+//         }
+
+//         return result;
+//     }
+// };
+
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0;
+
+        // Calculate the sum of the first 'k' elements
+        for (int i = 0; i < k; i++)
+            sum += nums[i];
+
+        double maxAverage = sum / k; // Initialize the maximum average
+
+        // Slide the window and calculate the maximum average
+        for (int i = k; i < nums.size(); i++) {
+            sum += nums[i] - nums[i - k]; // Add the next element and remove the first element of the window
+            maxAverage = max(maxAverage, sum / k); // Update the maximum average if necessary
+        }
+
+        return maxAverage;
+    }
+};
+
