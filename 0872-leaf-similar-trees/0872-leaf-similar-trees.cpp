0e67e473182e83f032714d36ba0fe981dfba628b
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-03 14:47:45 +0800

    Time: 3 ms (74.55%), Space: 12.6 MB (91.16%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -12,16 +12,21 @@
 
 class Solution {
 public:
-    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        return dfs(root1) == dfs(root2);
+    void dfs(TreeNode* root,vector<int> &v)
+    {
+        if(!root)
+            return ;
+        if(!root->left&&!root->right)
+            v.push_back(root->val);
+        dfs(root->left,v);
+        dfs(root->right,v);
     }
-
-    vector<int> dfs(TreeNode* root) {
-        if (!root) return {};
-        auto ans = dfs(root->left);
-        auto right = dfs(root->right);
-        ans.insert(ans.end(), right.begin(), right.end());
-        if (ans.empty()) ans.push_back(root->val);
-        return ans;
+    
+    bool leafSimilar(TreeNode* root1, TreeNode* root2)
+    {
+        vector<int> v1,v2;
+        dfs(root1,v1);
+        dfs(root2,v2);
+        return v1==v2;
     }
 };
