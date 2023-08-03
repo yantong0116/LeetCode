c7b45241cecae00573b0f84ab7454dce8fb5ff21
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-03 16:05:20 +0800

    Time: 20 ms (83.75%), Space: 15.8 MB (96.85%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -14,18 +14,18 @@ class Solution {
 public:
     int count = 0;
     
-    void solve(TreeNode* &root,long long int targetSum){
+    void solve(TreeNode* root,long long int targetSum){
         if(root==NULL) return;
         
         if(targetSum==root->val){
             count++;
         }
         
-        solve(root->left, targetSum-root->val);
-        solve(root->right, targetSum-root->val);
+        solve(root->left, targetSum - root->val);
+        solve(root->right, targetSum - root->val);
     }
     
-    int pathSum(TreeNode* &root, int &targetSum) {
+    int pathSum(TreeNode* root, int targetSum) {
         if(root == NULL) return 0;
         solve(root, targetSum);
         pathSum(root->left, targetSum);
