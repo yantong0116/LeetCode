8ecd3f0996e4084959b0179e6bd18274dba077c3
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-06 05:16:35 +0800

    Time: 4 ms (87.79%), Space: 5.8 MB (75.30%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,8 +1,7 @@
 int romanToInt(char *s) {
-    // Define the mapping of Roman numerals to integers using an array of pairs.
     char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int values[] = {1, 5, 10, 50, 100, 500, 1000};
-    int nums[128] = {0}; // Initialize an array to store integer values for each Roman numeral.
+    int nums[128] = {0}; 
 
     // Populate the nums array with the integer values.
     for (int i = 0; i < sizeof(numerals) / sizeof(numerals[0]); ++i) {
