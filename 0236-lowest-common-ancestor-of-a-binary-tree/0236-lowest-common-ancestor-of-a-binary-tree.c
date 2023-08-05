798add46840c0f00dd1fd0f0a08848d51e018908
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-05 19:22:55 +0800

    Time: 24 ms (71.07%), Space: 18.7 MB (44.65%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.c
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.c
@@ -7,15 +7,27 @@
  * };
  */
 
+// 尋找二叉樹節點的最低共同祖先
 struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
-    if(!root) return NULL;
-    if(root == p || root == q) return root;
-    struct TreeNode* left, * right;
-    left = lowestCommonAncestor(root->left, p, q);
-    right = lowestCommonAncestor(root->right, p, q);
-    if(left && right) return root;
-    if(left) return left;
-    if(right) return right;
+    // 如果節點為空，返回NULL
+    if (!root) return NULL;
+    
+    // 如果節點是p或q，則該節點為最低共同祖先
+    if (root == p || root == q) return root;
+    
+    // 在左子樹中尋找p和q的最低共同祖先
+    struct TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    
+    // 在右子樹中尋找p和q的最低共同祖先
+    struct TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    
+    // 如果p和q分別在當前節點的左右子樹中，則當前節點為最低共同祖先
+    if (left && right) return root;
+    
+    // 如果只有一個節點在左子樹或右子樹中，則返回該節點作為最低共同祖先
+    if (left) return left;
+    if (right) return right;
+    
+    // 如果p和q都不在當前節點的子樹中，返回NULL
     return NULL;
 }
-
