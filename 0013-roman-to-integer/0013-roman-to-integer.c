fad55beff5f200024552e9a6e885bd88bee873d0
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-06 05:26:27 +0800

    Time: 7 ms (69.77%), Space: 5.6 MB (96.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,17 +1,47 @@
-int romanToInt(char *s) {
-    char numerals[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-    int values[] = {1, 5, 10, 50, 100, 500, 1000};
-    int nums[128] = {0}; 
+// Simple hash map structure to store key-value pairs
+struct HashMap
+{
+    char key;
+    int value;
+};
 
-    // Populate the nums array with the integer values.
-    for (int i = 0; i < sizeof(numerals) / sizeof(numerals[0]); ++i) {
-        nums[numerals[i]] = values[i];
+// Function to get the value from the hash map based on the key
+int getValue(struct HashMap *map, int mapSize, char key)
+{
+    for (int i = 0; i < mapSize; i++)
+    {
+        if (map[i].key == key)
+            return map[i].value;
+    }
+    return -1; // Return -1 if the key is not found (this indicates an error in the Roman numeral)
+}
+
+int romanToInt(char *s)
+{
+    int ans = 0, n = strlen(s);
+    
+    // Define the hash map (equivalent to the unordered_map in C++)
+    struct HashMap mp[] = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int currentValue = getValue(mp, sizeof(mp) / sizeof(mp[0]), s[i]);
+        int nextValue = getValue(mp, sizeof(mp) / sizeof(mp[0]), s[i + 1]);
+
+        if (currentValue >= nextValue)
+            ans += currentValue;
+        else
+            ans -= currentValue;
     }
 
-    int ans = nums[s[strlen(s) - 1]];
-    for (int i = 0; i < strlen(s) - 1; ++i) {
-        int sign = nums[s[i]] < nums[s[i + 1]] ? -1 : 1;
-        ans += sign * nums[s[i]];
-    }
+    ans += getValue(mp, sizeof(mp) / sizeof(mp[0]), s[n - 1]);
     return ans;
 }
