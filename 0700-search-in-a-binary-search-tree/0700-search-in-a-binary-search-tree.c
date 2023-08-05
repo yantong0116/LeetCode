8a851cfbce2df01dfd64a333dd43c1bc33f8cb27
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-05 20:05:58 +0800

    Time: 23 ms (78.06%), Space: 15.2 MB (67.60%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.c
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.c
@@ -7,11 +7,10 @@
  * };
  */
 
-struct TreeNode* searchBST(struct TreeNode* root, int val) {
+struct TreeNode* searchBST(struct TreeNode* root, int val){
     if (root == NULL) return NULL;
     
-    if (root->val == val) return root;
-    else if (root->val > val) return searchBST(root->left, val);
+    if(root->val == val) return root;
+    else if(root->val > val) return searchBST(root->left, val);
     else return searchBST(root->right, val);
 }
-
