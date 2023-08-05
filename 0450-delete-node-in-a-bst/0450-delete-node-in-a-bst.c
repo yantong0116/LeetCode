f9bc95e883a5866c061c058dc565f61c910eb041
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-05 20:56:26 +0800

    Time: 16 ms (94.87%), Space: 13.7 MB (62.05%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.c
@@ -16,28 +16,32 @@ struct TreeNode* findMin(struct TreeNode* node) {
 
 struct TreeNode* deleteNode(struct TreeNode* root, int key) {
     if (!root) {
-        return NULL;
+        return NULL; // 當根節點為空，表示鍵值不存在於樹中，直接返回 NULL
     }
 
     if (key < root->val) {
-        root->left = deleteNode(root->left, key);
+        root->left = deleteNode(root->left, key); // 在左子樹中遞迴刪除鍵值節點
     } else if (key > root->val) {
-        root->right = deleteNode(root->right, key);
+        root->right = deleteNode(root->right, key); // 在右子樹中遞迴刪除鍵值節點
     } else {
+        // 找到了目標節點
         if (!root->left) {
+            // 目標節點沒有左子節點，將目標節點替換為右子節點
             struct TreeNode* temp = root->right;
-            free(root);
+            free(root); // 釋放目標節點的記憶體
             return temp;
         } else if (!root->right) {
+            // 目標節點沒有右子節點，將目標節點替換為左子節點
             struct TreeNode* temp = root->left;
-            free(root);
+            free(root); // 釋放目標節點的記憶體
             return temp;
         }
 
-        // Node with two children: Get the inorder successor (min value in the right subtree)
+        // 目標節點有兩個子節點
+        // 找到右子樹的最小值節點（或左子樹的最大值節點）
         struct TreeNode* temp = findMin(root->right);
-        root->val = temp->val;
-        root->right = deleteNode(root->right, temp->val);
+        root->val = temp->val; // 用最小值節點的值替換目標節點的值
+        root->right = deleteNode(root->right, temp->val); // 遞迴刪除最小值節點
     }
     return root;
 }
