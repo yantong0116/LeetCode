bf88d13bfd10fd5d171122c5c84731b2870a341b
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-06 05:30:11 +0800

    Time: 0 ms (100.00%), Space: 6 MB (15.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,47 +1,25 @@
-// Simple hash map structure to store key-value pairs
-struct HashMap
-{
-    char key;
-    int value;
-};
+int romanToInt(char* s) {
+    int romanValues[26]; // Create a HashMap to store the values of each Roman symbol
+    romanValues['I' - 'A'] = 1;
+    romanValues['V' - 'A'] = 5;
+    romanValues['X' - 'A'] = 10;
+    romanValues['L' - 'A'] = 50;
+    romanValues['C' - 'A'] = 100;
+    romanValues['D' - 'A'] = 500;
+    romanValues['M' - 'A'] = 1000;
 
-// Function to get the value from the hash map based on the key
-int getValue(struct HashMap *map, int mapSize, char key)
-{
-    for (int i = 0; i < mapSize; i++)
-    {
-        if (map[i].key == key)
-            return map[i].value;
-    }
-    return -1; // Return -1 if the key is not found (this indicates an error in the Roman numeral)
-}
+    int len = strlen(s);
+    int result = romanValues[s[len - 1] - 'A']; // Initialize result with the value of the last symbol
 
-int romanToInt(char *s)
-{
-    int ans = 0, n = strlen(s);
-    
-    // Define the hash map (equivalent to the unordered_map in C++)
-    struct HashMap mp[] = {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        int currentValue = getValue(mp, sizeof(mp) / sizeof(mp[0]), s[i]);
-        int nextValue = getValue(mp, sizeof(mp) / sizeof(mp[0]), s[i + 1]);
-
-        if (currentValue >= nextValue)
-            ans += currentValue;
-        else
-            ans -= currentValue;
+    for (int i = len - 2; i >= 0; i--) {
+        // If the value of the current symbol is less than the value of the next symbol,
+        // we need to subtract it from the result. Otherwise, we add it to the result.
+        if (romanValues[s[i] - 'A'] < romanValues[s[i + 1] - 'A']) {
+            result -= romanValues[s[i] - 'A'];
+        } else {
+            result += romanValues[s[i] - 'A'];
+        }
     }
 
-    ans += getValue(mp, sizeof(mp) / sizeof(mp[0]), s[n - 1]);
-    return ans;
+    return result;
 }
