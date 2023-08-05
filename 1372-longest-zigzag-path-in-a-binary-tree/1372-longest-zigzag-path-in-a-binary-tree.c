868c4ec8fa08e5df3b681dc717aeaf96f8867c77
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-05 13:49:36 +0800

    Time: 92 ms (95.24%), Space: 51.8 MB (83.33%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.c
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.c
@@ -7,22 +7,43 @@
  * };
  */
 
+void dfs(struct TreeNode* node, bool goLeft, int steps, int* pathLength) {
+    if (!node) return;
 
-#define LEFT -1
-#define RIGHT 1
-
-void DFS(struct TreeNode* root, int side, int cnt, int* ans){
-    if(!root) return;
-    if(cnt > *ans)
-        *ans = cnt;
-	/* check the preview node is left or right and decide the cnt add 1 or reset to 1 or not */
-    DFS(root->left, LEFT, side == LEFT? 1 : cnt + 1, ans);
-    DFS(root->right, RIGHT, side == RIGHT? 1 : cnt + 1, ans);
+    *pathLength = (*pathLength > steps) ? *pathLength : steps;
+    if (goLeft) {
+        dfs(node->left, false, steps + 1, pathLength);
+        dfs(node->right, true, 1, pathLength);
+    } else {
+        dfs(node->left, false, 1, pathLength);
+        dfs(node->right, true, steps + 1, pathLength);
+    }
 }
 
-int longestZigZag(struct TreeNode* root){
-    if(!root) return 0;
-    int ans = 0;
-    DFS(root, 0, 0, &ans);
-    return ans;
+int longestZigZag(struct TreeNode* root) {
+    if (!root) return 0;
+    int pathLength = 0;
+    dfs(root, false, 0, &pathLength);
+    dfs(root, true, 0, &pathLength);
+    return pathLength;
 }
+
+
+// #define LEFT -1
+// #define RIGHT 1
+
+// void DFS(struct TreeNode* root, int side, int cnt, int* ans){
+//     if(!root) return;
+//     if(cnt > *ans)
+//         *ans = cnt;
+
+//     DFS(root->left, LEFT, side == LEFT? 1 : cnt + 1, ans);
+//     DFS(root->right, RIGHT, side == RIGHT? 1 : cnt + 1, ans);
+// }
+
+// int longestZigZag(struct TreeNode* root){
+//     if(!root) return 0;
+//     int ans = 0;
+//     DFS(root, 0, 0, &ans);
+//     return ans;
+// }
