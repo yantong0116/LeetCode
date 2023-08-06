f218cdbf2c5449af941548ffe7164d1b9002be14
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-06 15:43:14 +0800

    Time: 0 ms (100.00%), Space: 6.2 MB (9.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.c
+++ 0066-plus-one/0066-plus-one.c
@@ -1,29 +1,22 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    int carry = 1; // 初始carry為1，因為要加1
-    int* result = (int*)malloc((digitsSize + 1) * sizeof(int)); // 預設多一位數，以防有進位
-    int resultIndex = digitsSize; // 結果陣列的索引
-
-    for (int i = digitsSize - 1; i >= 0; i--) {
-        int sum = digits[i] + carry;
-        result[resultIndex] = sum % 10; // 當前位的數字
-        carry = sum / 10; // 計算進位
-        resultIndex--;
-    }
-
-    // 檢查最後是否有進位，如果有，則將進位carry加到結果的最高位
-    if (carry) {
-        result[0] = carry;
-        *returnSize = digitsSize + 1;
-    } else {
-        *returnSize = digitsSize;
-        // 因為多預設了一位數，所以要將結果向右位移一格，讓最高位為0
-        for (int i = 0; i < digitsSize; i++) {
-            result[i] = result[i + 1];
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+     for(int i=digitsSize-1;i>=0;i--){
+        if(digits[i]==9){
+            digits[i]=0;
+        }
+        else{
+            digits[i]+=1;
+            * returnSize=digitsSize;
+            return digits;
         }
     }
-
+    
+    * returnSize=++digitsSize;
+    int* result = malloc(digitsSize * sizeof(int));
+    
+    for(int i=1;i<digitsSize;i++){
+        result[i]=0;
+    }
+    
+    result[0]=1;
     return result;
 }
