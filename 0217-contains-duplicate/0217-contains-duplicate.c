91f6df574d8c7ef23d330470dc1f372c1b3f4730
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-22 19:35:22 +0800

    Time: 103 ms (77.58%), Space: 14.1 MB (65.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.c
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -1,40 +1,15 @@
-void mergeSort(int *nums, int l, int r)
+int cmpfunc (int* a, int* b)
 {
-    if(l==r);
-    else
-    {
-        int mid = (l+r)/2;
-        mergeSort(nums, l, mid);
-        mergeSort(nums, mid+1, r);
-        int *temp = malloc((r-l+1)*sizeof(int));
-        
-        int a=l, b=mid+1, count=0;
-        while(a<=mid && b<=r)
-        {
-            if(nums[a]<=nums[b])
-            {
-                temp[count++]=nums[a++];
-            }
-            else if(nums[a]>nums[b])
-            {
-                temp[count++]=nums[b++];
-            }
-        }
-        while(a<=mid)temp[count++]=nums[a++];
-        while(b<=r)temp[count++]=nums[b++];
-        for(int i=0; i<count;i++)
-        {
-            nums[l+i]=temp[i];
-        }
-    }
+   return (*a-*b);
 }
 
 bool containsDuplicate(int* nums, int numsSize)
 {
-    mergeSort(nums, 0, numsSize-1);
-    for(int i=0; i<numsSize-1; i++)
-    {
-        if(nums[i]==nums[i+1])return true;
+    qsort(nums, numsSize, sizeof(int), cmpfunc);//(第一個指針的位置，大小，每個元素大小，比較函數(固定寫法)
+    for(int i = 0; i < numsSize - 1; i++){
+        if(nums[i] == nums[i + 1]){
+            return true;
+        }
     }
     return false;
 }
