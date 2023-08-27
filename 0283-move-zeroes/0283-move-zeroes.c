12e85be133d2f7d208aa650df00c8e95ba29d979
Yan <51469882+yantong0116@users.noreply.github.com>
2023-08-27 15:20:35 +0800

    Time: 59 ms (99.68%), Space: 15.2 MB (37.08%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,13 @@
+void moveZeroes(int* nums, int numsSize){
+    int i;
+    int next = 0;
+    
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[next++] = nums[i];
+        }
+    }
+    for (i = next; i < numsSize; i++) {
+        nums[i] = 0;
+    }
+}
