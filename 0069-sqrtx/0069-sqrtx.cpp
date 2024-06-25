ec6d1ba0c0e05bced572ac00838709a69ced38b1
Yan <51469882+yantong0116@users.noreply.github.com>
2024-06-25 23:13:27 +0800

    Time: 5 ms (34.62%), Space: 7.5 MB (40.06%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
-        if (x==0)return 0;
-        int low=1; int high= x;
-        
-        while (low <= high){
-            int mid= low+ (high-low)/2;
-            if (x % mid == 0 && mid == x / mid) return mid;
-            if (mid > x/mid){
-                high= mid-1;
-            }
-            else low= mid+1;
+        if(x < 2){
+            return x;
         }
-        
+
+        int low = 0;
+        int high = x;
+
+        while(low <= high){
+            long mid = (low + high) / 2;
+            if(mid * mid == x){
+                return mid;
+            }else if(mid * mid < x){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
         return high;
     }
 };
