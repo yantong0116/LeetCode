7059a2560dc542c89c4d0237779cc1e59c4de1a5
Yan <51469882+yantong0116@users.noreply.github.com>
2024-06-25 23:10:04 +0800

    Time: 5 ms (53.80%), Space: 5.7 MB (16.63%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,21 @@
+int mySqrt(int x) {
+    
+    if(x < 2){
+        return x;
+    }
+    
+    int low = 0;
+    int high = x;
+    
+    while(low <= high){
+        long mid = (low + high) / 2;
+        if(mid * mid == x){
+            return mid;
+        }else if(mid * mid < x){
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return high;
+}
